c6974ceb3e393efa2f3708ee8851c5aa9553e3a1
PATNAM DHANUSH <patnamdhanush@DHANU-MacBook-Air.local>
2024-09-22 00:29:28 +0530

    GPIOF_added

--- main.c
+++ main.c
@@ -11,5 +11,13 @@ int main(void)
     
     while(1)
     {
+
     }
+}
+
+
+void GPIOF_Handler(void)
+{
+    GPIO_PORTF_ICR_R |= 0x10;             /* Clear interrupt flag for PF4 */
+    GPIO_PORTF_DATA_R ^= 0x02;            /* Toggle the RED LED (PF1) */
 }
