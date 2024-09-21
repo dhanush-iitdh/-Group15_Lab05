517b1f99e26523b234d39da3dd8fb4cd990aee24
dhanush-iitdh <ee24mt010@iitdh.ac.in>
2024-09-22 00:24:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "tm4c123gh6pm.h"
 
+void GPIOF_Handler(void);
+void GPIO_PORTF_Init(void);
 
-/**
- * main.c
- */
 int main(void)
 {
-	return 0;
+    GPIO_PORTF_Init();  /* Initialize GPIO Port F */
+    
+    while(1)
+    {
+    }
 }
