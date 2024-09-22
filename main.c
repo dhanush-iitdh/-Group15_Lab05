c8081bf9634ed1eab5344f0f0dfa64523847ebc9
Vipul Verma <66062033+vip02aug@users.noreply.github.com>
2024-09-22 12:20:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,12 +7,18 @@ void GPIO_PORTF_Init(void);
 
 int main(void)
 {
+    SYSCTL_RCGCGPIO_R |= 0x020;
+              GPIO_PORTF_LOCK_R= 0x4C4F4348;
+              GPIO_PORTF_CR_R |= 0x10;
+              GPIO_PORTF_DIR_R = 0x02;
+              GPIO_PORTF_DEN_R = 0x12;
+              GPIO_PORTF_PUR_R = 0x10;
     GPIO_PORTF_Init();  /* Initialize GPIO Port F */
     
     while(1)
     {
 
-    }ij
+    }
 }
 
 
