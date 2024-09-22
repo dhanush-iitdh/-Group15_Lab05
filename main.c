6bee8d1996aea060736ebdd4d5e699068f4589e8
Vipul Verma <66062033+vip02aug@users.noreply.github.com>
2024-09-22 12:24:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -20,13 +20,14 @@ void init(void)
 
 int main()
 {
-    SYSCTL_RCGCGPIO_R |= 0x020;
-              GPIO_PORTF_LOCK_R= 0x4C4F4348;
-              GPIO_PORTF_CR_R |= 0x10;
-              GPIO_PORTF_DIR_R = 0x02;
-              GPIO_PORTF_DEN_R = 0x12;
-              GPIO_PORTF_PUR_R = 0x10;
-    GPIO_PORTF_Init();  /* Initialize GPIO Port F */
+init();
+    GPIO_PORTF_DATA_R = 0x00; //Making red led to be off initially
+               while(1)
+               {
+                  // do nothing
+               }
+   
+}
     
     while(1)
     {
